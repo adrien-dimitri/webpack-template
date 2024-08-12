88bf69cf49d00607a315ed936c1ef5884baa61c6
Adrien <amaugueret@gmail.com>
2024-08-12 14:42:20 +0200

    added browser to languages

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,9 +2,15 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
 
-
 export default [
-  {languageOptions: { globals: globals.node }},
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+      },
+    },
+  },
   pluginJs.configs.recommended,
   eslintConfigPrettier,
 ];
